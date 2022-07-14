0504f25a4f63f563c8f85310c19ed8075b13cee9
Mulya Nurdin <mulyanurdin10@gmail.com>
2022-07-14 16:07:45 +0700

    change to main

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"be9/mnroom/config"
+	"be9/mnroom/factory"
+	_middlewares "be9/mnroom/middlewares"
+	"be9/mnroom/migration"
+	"be9/mnroom/routes"
+)
 
+func main() {
+	dbConn := config.InitDB()
+	migration.InitMigrate(dbConn)
+	presenter := factory.InitFactory(dbConn)
+	e := routes.New(presenter)
+	_middlewares.LogMiddlewares(e)
+	e.Logger.Fatal(e.Start(":8000"))
 }
