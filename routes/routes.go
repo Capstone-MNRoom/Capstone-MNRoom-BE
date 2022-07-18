5fed10c73336ecf0f87b4512d5ab60049b1c01ac
David Wahyu Pratomo <22112331+davidwah@users.noreply.github.com>
2022-07-18 16:21:05 +0700

    remove jwt endpoint

--- routes/routes.go
+++ routes/routes.go
@@ -47,8 +47,8 @@ func New(presenter factory.Presenter) *echo.Echo {
 
 	// Rooms
 	e.POST("/rooms", presenter.RoomPresenter.InsertData, _middlewares.JWTMiddleware())
-	e.GET("/rooms", presenter.RoomPresenter.GetDataAll, _middlewares.JWTMiddleware())
-	e.GET("/rooms/:id", presenter.RoomPresenter.GetData, _middlewares.JWTMiddleware())
+	e.GET("/rooms", presenter.RoomPresenter.GetDataAll)
+	e.GET("/rooms/:id", presenter.RoomPresenter.GetData)
 	e.PUT("/rooms/:id", presenter.RoomPresenter.UpdateData, _middlewares.JWTMiddleware())
 	e.DELETE("/rooms/:id", presenter.RoomPresenter.DeleteData, _middlewares.JWTMiddleware())
 
