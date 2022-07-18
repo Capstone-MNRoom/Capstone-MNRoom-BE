60fd030725e8c466e972394be85b21d20a53a9fa
davidwah <davidwahyuyu@gmail.com>
2022-07-18 09:46:59 +0700

    remove jwt on Room-GetAll

--- routes/routes.go
+++ routes/routes.go
@@ -46,7 +46,7 @@ func New(presenter factory.Presenter) *echo.Echo {
 
 	// Rooms
 	e.POST("/rooms", presenter.RoomPresenter.InsertData, _middlewares.JWTMiddleware())
-	e.GET("/rooms", presenter.RoomPresenter.GetAllData, _middlewares.JWTMiddleware())
+	e.GET("/rooms", presenter.RoomPresenter.GetAllData)
 	e.GET("/rooms/:id", presenter.RoomPresenter.GetData, _middlewares.JWTMiddleware())
 	e.PUT("/rooms/:id", presenter.RoomPresenter.UpdateData, _middlewares.JWTMiddleware())
 	e.DELETE("/rooms/:id", presenter.RoomPresenter.DeleteData, _middlewares.JWTMiddleware())
