b909511fb6298fc211c2da96fd7e827cf870646c
davidwah <davidwahyuyu@gmail.com>
2022-07-22 16:13:42 +0700

    create testing users

--- features/users/business/usecase_test.go
+++ features/users/business/usecase_test.go
@@ -1,188 +1,94 @@
 package business
 
-// import (
-// 	"be9/mnroom/features/users"
-// 	"fmt"
-// 	"testing"
-// 	// "time"
+import (
+	"be9/mnroom/features/users"
+	"fmt"
+	"testing"
 
-// 	// "github.com/stretchr/testify/assert"
-// )
+	"github.com/stretchr/testify/assert"
+	// "be9/mnroom/features/users"
+	// "fmt"
+	// "testing"
+	// "github.com/stretchr/testify/assert"
+)
 
-// type mockUserData struct{}
+type mockUserData struct{}
 
-// func (mock mockUserData) PostUser(data users.Core) (row int, err error) {
-// 	return 1, nil
+// InsertData(insert Core) (row int, err error)
+func (mock mockUserData) InsertData(data users.Core) (row int, err error) {
+	return 1, nil
+}
+
+// func (mock mockUserDataSuccess) AuthUser(email string, password string) (username string, token string, err error) {
+// 	return "mulya", "tokenmulyatokenmulyatokenmulyatokenmulya", nil
+// }
+func (mock mockUserData) UpdateData(id int, data users.Core) (row int, err error) {
+	return 1, nil
+}
+
+func (mock mockUserData) GetData(id int) (data users.Core, err error) {
+	return users.Core{
+		ID:       1,
+		Image:    "https://storage.googleapis.com/profile/profile_default.png",
+		Username: "Mulya Nurdin",
+		Email:    "mulya@mail.com",
+		Phone:    "081234567890",
+		Address:  "Bandung",
+	}, nil
+}
+func (mock mockUserData) DeleteData(id int) (row int, err error) {
+	return 1, nil
+}
+
+type mockUserDataFailed struct {
+}
+
+func (mock mockUserDataFailed) InsertData(data users.Core) (row int, err error) {
+	return 0, fmt.Errorf("failed insert data")
+}
+
+// func (mock mockUserDataFailed) AuthUser(email string, password string) (username string, token string, err error) {
+// 	return "", "", fmt.Errorf("failed")
+// }
+func (mock mockUserDataFailed) UpdateData(id int, data users.Core) (row int, err error) {
+	return 0, fmt.Errorf("failed insert data")
+}
+func (mock mockUserDataFailed) GetUser(id int) (data users.Core, err error) {
+	return users.Core{}, fmt.Errorf("error")
+}
+func (mock mockUserDataFailed) DeleteData(id int) (row int, err error) {
+	return 0, nil
+}
+
+// type Business interface {
+// 	InsertData(insert Core) (row int, err error)
+// 	GetAllData(limit int, offset int) (data []Core, err error)
+// 	GetData(id int) (data Core, err error)
+// 	DeleteData(id int) (row int, err error)
+// 	UpdateData(id int, insert Core) (row int, err error)
 // }
 
-// func (mock mockUserData) AuthUser(email string, password string) (userName string, token string, e error) {
-// 	return "andri", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9", nil
-// }
+func TestInsertData(t *testing.T) {
+	t.Run("Test insert data user", func(t *testing.T) {
+		userBusiness := NewUserBusiness(mockUserData{})
+		newUser := users.Core{
+			Username: "Mulya Nurdin",
+			Email:    "mei@mail.com",
+			Password: "qwerty1234",
+			Phone:    "031123456",
+			Address:  "Bandung",
+		}
+		result, err := userBusiness.InsertData(newUser)
+		assert.Nil(t, err)
+		assert.Equal(t, 1, result)
+	})
+}
 
-// func (mock mockUserData) PutDataUser(id int, data users.Core) (row int, err error) {
-// 	return 1, nil
-// }
-
-// func (mock mockUserData) GetUser(id int) (data users.Core, err error) {
-// 	return users.Core{
-// 		ID: 1, Username: "Mulya Nurdin", Email: "mulya@mail.com", PhoneNumber
-// 		//Username: "Andri G", UserName: "Andri", Email: "andri@gmail.com", PhoneNumber: "081234", Address: "Jln. Urip", ImageURL: "https://storage.com/profile.png", CreatedAt: time.Time{},
-// 	}, nil
-// }
-
-// func (mockUserData) DeleteUser(id int) (row int, err error) {
-// 	return 1, nil
-// }
-
-// type mockUserDataFailed struct{}
-
-// func (mock mockUserDataFailed) PostUser(data users.Core) (row int, err error) {
-// 	return 0, fmt.Errorf("Failed to select data")
-// }
-
-// func (mock mockUserDataFailed) AuthUser(email string, password string) (userName string, token string, e error) {
-// 	return "", "", fmt.Errorf("email or password incorrect")
-// }
-
-// func (mock mockUserDataFailed) PutDataUser(id int, data users.Core) (row int, err error) {
-// 	return 0, fmt.Errorf("failed to update user")
-// }
-
-// func (mock mockUserDataFailed) GetUser(id int) (data users.Core, err error) {
-// 	return users.Core{}, fmt.Errorf("failed to get data")
-// }
-
-// func (mockUserDataFailed) DeleteUser(id int) (row int, err error) {
-// 	return 0, nil
-// }
-
-// func TestInsertUser(t *testing.T) {
-// 	t.Run("Test Insert User Success", func(t *testing.T) {
-// 		userBusiness := NewUserBusiness(mockUserData{})
-// 		newUser := users.Core{
-// 			Us:    "Andri",
-// 			Email:       "andri@gmail.com",
-// 			Password:    "qwerty",
-// 			PhoneNumber: "081234",
-// 			Address:     "Jalan Urip",
-// 			ImageURL:    "profile.png",
-// 		}
-// 		result, err := userBusiness.InsertUser(newUser)
-// 		assert.Nil(t, err)
-// 		assert.Equal(t, 1, result)
-// 	})
-
-// 	t.Run("Test Insert Data Failed", func(t *testing.T) {
-// 		userBusiness := NewUserBusiness(mockUserDataFailed{})
-// 		newUser := users.Core{
-// 			FullName:    "Andri G",
-// 			UserName:    "Andri",
-// 			Email:       "andri@gmail.com",
-// 			Password:    "qwerty",
-// 			PhoneNumber: "081234",
-// 			Address:     "Jalan Urip",
-// 			ImageURL:    "profile.png",
-// 		}
-// 		result, err := userBusiness.InsertUser(newUser)
-// 		assert.NotNil(t, err)
-// 		assert.Equal(t, 0, result)
-// 	})
-// 	t.Run("Test Insert Data Failed When Full Name Empty", func(t *testing.T) {
-// 		userBusiness := NewUserBusiness(mockUserDataFailed{})
-// 		newUser := users.Core{
-// 			UserName:    "Andri",
-// 			Email:       "andri@gmail.com",
-// 			Password:    "qwerty",
-// 			PhoneNumber: "081234",
-// 			Address:     "Jalan Urip",
-// 			ImageURL:    "profile.png",
-// 		}
-// 		result, err := userBusiness.InsertUser(newUser)
-// 		assert.NotNil(t, err)
-// 		assert.Equal(t, -1, result)
-// 	})
-// }
-
-// func TestLoginUser(t *testing.T) {
-// 	t.Run("Test Login User Success", func(t *testing.T) {
-// 		userBusiness := NewUserBusiness(mockUserData{})
-// 		var loginEmail = "andri@gmail.com"
-// 		var loginPass = "qwerty"
-
-// 		resUserName, resToken, err := userBusiness.LoginUser(loginEmail, loginPass)
-// 		assert.Nil(t, err)
-// 		assert.Equal(t, "andri", "andri", resUserName, resToken)
-// 	})
-
-// 	t.Run("Test Login User Failed", func(t *testing.T) {
-// 		userBusiness := NewUserBusiness(mockUserDataFailed{})
-// 		var loginEmail = "andri@gmail.com"
-// 		var loginPass = "qwerty"
-
-// 		resUserName, resToken, err := userBusiness.LoginUser(loginEmail, loginPass)
-// 		assert.NotNil(t, err)
-// 		assert.Equal(t, "andri", "andri", resUserName, resToken)
-// 	})
-// }
-
-// func TestUpdateDataUser(t *testing.T) {
-// 	t.Run("Test Update Data User Succes", func(t *testing.T) {
-// 		userBusiness := NewUserBusiness(mockUserData{})
-// 		editUser := users.Core{
-// 			FullName:    "Andri G",
-// 			UserName:    "Andri",
-// 			Email:       "andri@gmail.com",
-// 			Password:    "qwerty",
-// 			PhoneNumber: "081234",
-// 			Address:     "Jalan Urip",
-// 			ImageURL:    "profile1.png",
-// 		}
-// 		result, err := userBusiness.UpdateDataUser(0, editUser)
-// 		assert.Nil(t, err)
-// 		assert.Equal(t, 1, result)
-// 	})
-// 	t.Run("Test Update Data User Failed", func(t *testing.T) {
-// 		userBusiness := NewUserBusiness(mockUserDataFailed{})
-// 		editUser := users.Core{
-// 			UserName:    "Andri",
-// 			Email:       "andri@gmail.com",
-// 			Password:    "qwerty",
-// 			PhoneNumber: "081234",
-// 			Address:     "Jalan Urip",
-// 			ImageURL:    "profile1.png",
-// 		}
-// 		result, err := userBusiness.UpdateDataUser(0, editUser)
-// 		assert.NotNil(t, err)
-// 		assert.Equal(t, -1, result)
-// 	})
-// }
-
-// func TestSelectUser(t *testing.T) {
-// 	t.Run("Test Select User Success", func(t *testing.T) {
-// 		userBusiness := NewUserBusiness(mockUserData{})
-// 		result, err := userBusiness.SelectUser(0)
-// 		assert.Nil(t, err)
-// 		assert.Equal(t, "Andri G", result.FullName)
-// 	})
-// 	t.Run("Test Select User Failed", func(t *testing.T) {
-// 		userBusiness := NewUserBusiness(mockUserDataFailed{})
-// 		result, err := userBusiness.SelectUser(0)
-// 		assert.NotNil(t, err)
-// 		assert.Equal(t, users.Core{}, result)
-// 	})
-// }
-
-// func TestDeleteUser(t *testing.T) {
-// 	t.Run("Test Delete User Succes", func(t *testing.T) {
-// 		userBusiness := NewUserBusiness(mockUserData{})
-// 		result, err := userBusiness.DeleteUser(0)
-// 		assert.Nil(t, err)
-// 		assert.Equal(t, 1, result)
-// 	})
-// 	t.Run("Test Delete User Failed", func(t *testing.T) {
-// 		userBusiness := NewUserBusiness(mockUserDataFailed{})
-// 		result, err := userBusiness.DeleteUser(0)
-// 		assert.Nil(t, err)
-// 		assert.Equal(t, 0, result)
-// 	})
-// }
+func TestGetData(t *testing.T) {
+	t.Run("Test get data user", func(t *testing.T) {
+		userBusiness := NewUserBusiness(mockUserData)
+		result, err := userBusiness.GetData(0)
+		assert.Nil(t, err)
+		assert.Equal(t, "mulya", result.Username)
+	})
+}
