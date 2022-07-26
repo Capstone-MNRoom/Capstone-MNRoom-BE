9dcc0f040259d4942a74dd6096e1b9941ba7011b
davidwah <davidwahyuyu@gmail.com>
2022-07-26 22:42:42 +0700

    create mock testing

---
+++ features/users/business/usecase_test.go
@@ -0,0 +1,81 @@
+package business
+
+import (
+	"be9/mnroom/features/users"
+	"be9/mnroom/mocks"
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestInsertData(t *testing.T) {
+	repo := new(mocks.UserData)
+	insertData := users.Core{ID: 1, 
+		Username: "alta", 
+		Email: "alta@mail.id", 
+		Password: "qwerty", 
+		
+		Address: "Jl.Rambutan"}
+	// returnData := users.Core{ID: 1, Name: "alta", Email: "alta@mail.id", Password: "qwerty"}
+
+	t.Run("Success Insert", func(t *testing.T) {
+		repo.On("Test InsertData", mock.Anything).Return(1, nil).Once()
+		srv := NewUserBusiness(repo)
+
+		res, err := srv.InsertData(insertData)
+		assert.NoError(t, err)
+		assert.Equal(t, 1, res)
+		repo.AssertExpectations(t)
+	})
+
+	t.Run("Error insert to DB", func(t *testing.T) {
+		repo.On("Test InsertData", mock.Anything).Return(0, errors.New("there is some error")).Once()
+		srv := NewUserBusiness(repo)
+
+		res, err := srv.InsertData(insertData)
+		assert.EqualError(t, err, "there is some error")
+		assert.Equal(t, 0, res)
+		repo.AssertExpectations(t)
+	})
+
+	// t.Run("Error insert when incomplete data", func(t *testing.T) {
+	// 	/*
+	// 		dont need to write repo.On because this test case dont need to call data layer. just handle on business layer.
+	// 	*/
+	// 	// repo.On("InsertData", mock.Anything).Return(-1, errors.New("all input data must be filled")).Once()
+	// 	srv := NewUserBusiness(repo)
+
+	// 	_, err := srv.InsertData(users.Core{})
+	// 	assert.EqualError(t, err, "all input data must be filled")
+	// 	repo.AssertExpectations(t)
+	// })
+}
+
+// func TestGetAllData(t *testing.T) {
+// 	repo := new(mocks.UserData)
+// 	returnData := []users.Core{{ID: 1, Username: "Mulya", Email: "mulya@mail.com", Password: "123456", Phone: "08995555", Address: "Jl.Rambutan", Image: "https://storage.googleapis.com/event2022/Logo.png"}}
+
+// 	t.Run("Success Get All", func(t *testing.T) {
+// 		repo.On("GetAllData", mock.Anything).Return(returnData, nil).Once()
+
+// 		srv := NewUserBusiness(repo)
+
+// 		res, err := srv.GetAllData()
+// 		assert.NoError(t, err)
+// 		assert.Equal(t, returnData[0].ID, res[0].ID)
+// 		repo.AssertExpectations(t)
+// 	})
+
+// 	t.Run("Error Get All", func(t *testing.T) {
+// 		repo.On("SelectData", "").Return(nil, errors.New("data not found")).Once()
+
+// 		srv := NewUserBusiness(repo)
+
+// 		res, err := srv.GetAllData()
+// 		assert.Error(t, err)
+// 		assert.Nil(t, res)
+// 		repo.AssertExpectations(t)
+// 	})
+// }
--- go.mod
+++ go.mod
@@ -19,6 +19,7 @@ require (
 	cloud.google.com/go v0.103.0 // indirect
 	cloud.google.com/go/compute v1.7.0 // indirect
 	cloud.google.com/go/iam v0.3.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
@@ -35,6 +36,9 @@ require (
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.4.0 // indirect
+	github.com/stretchr/testify v1.8.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
 	go.opencensus.io v0.23.0 // indirect
@@ -48,4 +52,5 @@ require (
 	google.golang.org/grpc v1.48.0 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
